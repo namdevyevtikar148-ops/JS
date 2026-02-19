f3cd7a3aa28668930ce5e36d6343cbf636ff5193
Namdev Yevtikar <namdevyevtikar148@gmail.com>
2026-02-19 22:50:26 +0530

    added

---
+++ Basics/operations.js
@@ -0,0 +1,12 @@
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+console.log(2+"3");
+console.log("1"+2+2);
+console.log("2"+1+1);
+console.log(1+2+"2");
+console.log(+true);
+//mdn
+
+
